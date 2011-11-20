490fcf2408d538f1fd1bf2d13cb44bb690464cf3
Michal Kuncl <michal.kuncl@gmail.com>
2011-11-20 09:21:55 +0000

    git-svn-id: https://pavucina.dyndns.org/svn/Torchiver@2 f63cbac5-e01c-1f43-9cb5-bfa0d523cfdd

---
+++ VisualStudio/Torchiver/Torchiver.Archiver/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení jsou řízeny přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Torchiver.Archiver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Torchiver.Archiver")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení atributu ComVisible na hodnotu False udělá typy v tomto sestavení neviditelné 
+// pro komponenty modelu COM.  Jestliže potřebujete přistupovat k typům v tomto sestavení z 
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující identifikátor GUID slouží k identifikaci knihovny typů, jestliže je projekt vystaven objektům modelu COM
+[assembly: Guid("e914ae7e-5d22-4d16-8efa-8cc198173df7")]
+
+// Informace o verzi sestavení se skládá z následujících čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze 
+//      Číslo sestavení
+//      Revize
+//
+// Lze zadat všechny hodnoty nebo nechat hodnoty Čísel Sestavení a Revize ve výchozím nastavení 
+// použitím '*' jak je ukázáno dále:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VisualStudio/Torchiver/Torchiver.TempMover/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení jsou řízeny přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Torchiver.TempMover")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Torchiver.TempMover")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení atributu ComVisible na hodnotu False udělá typy v tomto sestavení neviditelné 
+// pro komponenty modelu COM.  Jestliže potřebujete přistupovat k typům v tomto sestavení z 
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující identifikátor GUID slouží k identifikaci knihovny typů, jestliže je projekt vystaven objektům modelu COM
+[assembly: Guid("e1b9da84-df25-49f3-84cf-a425a59201a8")]
+
+// Informace o verzi sestavení se skládá z následujících čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze 
+//      Číslo sestavení
+//      Revize
+//
+// Lze zadat všechny hodnoty nebo nechat hodnoty Čísel Sestavení a Revize ve výchozím nastavení 
+// použitím '*' jak je ukázáno dále:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
