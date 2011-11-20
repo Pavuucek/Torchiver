17484097338729b6775d517af94023f9dea0c1e6
Michal Kuncl <michal.kuncl@gmail.com>
2011-11-20 10:26:55 +0000

    git-svn-id: https://pavucina.dyndns.org/svn/Torchiver@5 f63cbac5-e01c-1f43-9cb5-bfa0d523cfdd

--- VisualStudio/Torchiver/Torchiver.TempMover/Forms/MainForm.Designer.cs
+++ VisualStudio/Torchiver/Torchiver.TempMover/Forms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Torchiver.TempMover
+﻿namespace Torchiver.TempMover.Forms
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Vyžadovaná proměnná návrháře.
@@ -30,12 +30,12 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(707, 297);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(680, 297);
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
--- VisualStudio/Torchiver/Torchiver.TempMover/Program.cs
+++ VisualStudio/Torchiver/Torchiver.TempMover/Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-
+using Torchiver.TempMover.Forms;
+using ArachNGIN.Files;
 namespace Torchiver.TempMover
 {
     static class Program
     {
+        public static MainForm MF;
+        public static TempManager TempM;
         /// <summary>
         /// Hlavní vstupní bod aplikace.
         /// </summary>
@@ -15,7 +18,7 @@ namespace Torchiver.TempMover
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(MF = new MainForm());
         }
     }
 }
