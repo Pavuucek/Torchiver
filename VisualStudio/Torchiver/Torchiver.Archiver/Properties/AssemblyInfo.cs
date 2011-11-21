ce56d8ca2706ed2cb2b50ada126b5e9f5e657cde
Michal Kuncl <michal.kuncl@gmail.com>
2011-11-21 16:54:42 +0000

    git-svn-id: https://pavucina.dyndns.org/svn/Torchiver@11 f63cbac5-e01c-1f43-9cb5-bfa0d523cfdd

--- VisualStudio/Torchiver/Torchiver.Archiver/Properties/AssemblyInfo.cs
+++ VisualStudio/Torchiver/Torchiver.Archiver/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Lze zadat všechny hodnoty nebo nechat hodnoty Čísel Sestavení a Revize ve výchozím nastavení 
 // použitím '*' jak je ukázáno dále:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
