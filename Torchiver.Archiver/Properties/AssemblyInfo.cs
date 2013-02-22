182e9a92311272e13a97679274c8520af5b02e6b
Michal Kuncl <michal.kuncl@gmail.com>
2013-02-22 21:37:01 +0100

    test

--- Torchiver.Archiver/Properties/AssemblyInfo.cs
+++ Torchiver.Archiver/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Lze zadat všechny hodnoty nebo nechat hodnoty Čísel Sestavení a Revize ve výchozím nastavení 
 // použitím '*' jak je ukázáno dále:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.146")]
+[assembly: AssemblyVersion("1.0.0.148")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
